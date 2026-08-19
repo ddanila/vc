c97e62bcb2c2b381aa702509185df6f9c1d8cf0b
Danila Sukharev <dan.sukharev@gmail.com>
2026-08-19 21:04:02 +0300

    Pin VC menu sort

--- tests/test_user_menu_contract.c
+++ tests/test_user_menu_contract.c
@@ -86,6 +86,8 @@ static void test_exact_user_menu(void) {
 
   check(write_user_menu(), "created deterministic VC.MNU fixture");
   usleep(300000);
+  kviktest_send_key(0x6000);  /* Ctrl-F3: pin source sort to Name */
+  usleep(300000);
   capture(&baseline);
 
   kviktest_send_key(KEY_F2);
